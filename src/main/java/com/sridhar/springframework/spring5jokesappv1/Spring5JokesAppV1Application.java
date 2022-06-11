f7916e77be83e4c69171633b752e14607c23ad30
meetsridhar9 <meetsridhar9@gmail.com>
2022-06-11 12:08:03 -0700

    Initial proj commit

---
+++ src/main/java/com/sridhar/springframework/spring5jokesappv1/Spring5JokesAppV1Application.java
@@ -0,0 +1,13 @@
+package com.sridhar.springframework.spring5jokesappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sridhar/springframework/spring5jokesappv1/Spring5JokesAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sridhar.springframework.spring5jokesappv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
